e01a707267095e5bfce01c96c35b8323015a653e
Filip Adamczyk <filip.adamczyk90@gmail.com>
2019-05-16 10:42:31 +0200

    walking through the dark side of the moon with symfony

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,26 +5,45 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('omg');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-    * @Route ("/home/{slug}")
+    * @Route ("/home/{slug}", name="article_show")
     */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page: %s',
-            $slug
-        ));
+        $comments = [
+            'I ate a pork',
+            'Dupa dupa duPA',
+            'lubie placki JHEaJSID',
+        ];
+
+
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-','', $slug)),
+            'comments' => $comments,
+        ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug)
+    {
+        // TODO - actually heart/unheart the article
+
+        return new JsonResponse(['hearts' => rand(5,100)]);
     }
 
 }
